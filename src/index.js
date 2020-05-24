51f9f705a4883ce27f7ddf933a98a6289a162385
Enrico Mrass <emrass@users.noreply.github.com>
2020-05-24 15:36:47 -0700

    Taking Turns - https://reactjs.org/tutorial/tutorial.html#taking-turns

--- src/index.js
+++ src/index.js
@@ -14,14 +14,18 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice(); // create a copy of squares for immutability
-    squares[i] = 'X';
-    this.setState({squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -34,7 +38,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <div>
